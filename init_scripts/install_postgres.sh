603a4b2e5932a5f92874982109d7edf35b792c37
gabrber <gabrielaberecka@gmail.com>
2019-01-05 16:32:29 +0100

    prepare csv files

--- init_scripts/install_postgres.sh
+++ init_scripts/install_postgres.sh
@@ -2,3 +2,5 @@
 
 apt-get update
 apt-get install -y postgresql postgresql-contrib pgadmin3
+
+pip3 install pandas
---
+++ init_scripts/prepare_data.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import glob
+import os
+
+# POI
+
+poifile = "../data/rows.csv?accessType=DOWNLOAD"
+poi=pd.read_csv(poifile)
+keep_col = ['PLACEID','NAME','FACILITY_T','FACI_DOM','the_geom']
+tmp_poi = poi[keep_col]
+tmp_poi['POI_LONGITUDE'] = ""
+tmp_poi['POI_LATITUDE'] = ""
+
+for i, row in tmp_poi.iterrows():
+  poi_point = str(row['the_geom']).split(" ")
+  poi_longitude = str(poi_point[1]).replace("(","")
+  poi_latitude = str(poi_point[2]).replace(")","")
+
+  tmp_poi.at[i,'POI_LONGITUDE'] = poi_longitude
+  tmp_poi.at[i,'POI_LATITUDE'] = poi_longitude
+
+new_poi = tmp_poi[['PLACEID','NAME','FACILITY_T','FACI_DOM','POI_LONGITUDE','POI_LATITUDE']]
+
+new_poi.to_csv("../data/poi.csv", index=False)
+#os.remove(poifile)
+
+# Green Taxi
+
+taxi_files = glob.glob('../data/green*')
+
+for i, green_csv in enumerate(taxi_files):
+
+  toedit = open(green_csv).read()
+  toedit = toedit.replace(',,', '')
+  edited = open(green_csv, 'w')
+  edited.write(toedit)
+  edited.close()
+
+  keep_col = ['lpep_pickup_datetime','Lpep_dropoff_datetime','Pickup_longitude','Pickup_latitude','Dropoff_longitude','Dropoff_latitude', 'Passenger_count']
+  taxi=pd.read_csv(green_csv,usecols=keep_col)
+
+  new_taxi = taxi[keep_col]
+  new_taxi['Pickup_znoe'] = ""
+  new_taxi['Dropoff_zone'] = ""
+  new_taxi['Dropoff_poi'] = ""
+
+  filename = "../data/green-" + str(i)
+  new_taxi.to_csv(filename, index=False)
+
+  #os.remove(green_csv)
