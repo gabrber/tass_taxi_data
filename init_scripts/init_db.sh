82571bc5047e08456a55680d83c5ee3801b1be31
gabrber <gabrielaberecka@gmail.com>
2019-01-07 00:06:18 +0100

    change password

--- init_scripts/init_db.sh
+++ init_scripts/init_db.sh
@@ -4,6 +4,7 @@ DATABASE="tass"
 
 #sudo -u postgres createdb $DATABASE
 sudo -u postgres psql -d $DATABASE -f sql/create_tables.sql
+sudo -u postgres psql -d $DATABASE -c "ALTER USER postgres WITH PASSWORD 'psql';"
 
-#cat ../data/green-* | sudo -u postgres psql -d $DATABASE -c "COPY green_taxi FROM stdin WITH CSV HEADER;"
+cat ../data/green-* | sudo -u postgres psql -d $DATABASE -c "COPY green_taxi FROM stdin WITH CSV HEADER;"
 cat ../data/poi* | sudo -u postgres psql -d $DATABASE -c "COPY poi FROM stdin WITH CSV HEADER;"
