192dbf6ef3dced1204f32ea64d10b40e43f5dd51
kkpalczewski <firstcalmly@gmail.com>
2019-01-06 22:39:04 +0100

    Add table taxi_zones

--- processing_scripts/calculate_zone.py
+++ processing_scripts/calculate_zone.py
@@ -74,7 +74,6 @@ def calculate_zones_for_dropoff(conn, curs):
 
 def calculate_zones_for_poi(conn):
 
-
     # we get name of tables for taxi drives and taxi zones
     taxi_zones = get_info.get_tables_pattern("zones", conn)
     pois = get_info.get_tables_pattern("gpoi", conn)
@@ -90,15 +89,33 @@ def calculate_zones_for_poi(conn):
     taxi_zone_data = pd.read_sql(taxi_zone_sql, conn)
     taxi_zone_data['newgeom'] = taxi_zone_data['newgeom'].apply(wkt.loads)
 
+    for poi_name in pois:
+        # populate point for dropoff
+        taxi_drives_dropoff_sql = "SELECT ST_AsText(ST_Transform(\"Dropoff_point\", 4326)) as newgeom,* FROM " + taxi_drives_name + " limit 5;"
+        taxi_drives_dropoff_data = pd.read_sql(taxi_drives_dropoff_sql, conn)
+        taxi_drives_dropoff_data['newgeom'] = taxi_drives_dropoff_data['newgeom'].apply(wkt.loads)
+
+        for index1, zone in taxi_zone_data[['newgeom','gid']].iterrows():
+            for index2, point in taxi_drives_dropoff_data[['newgeom', 'id']].iterrows():
+                if zone['newgeom'].contains(point['newgeom']) == True:
+                    sql = "UPDATE " + taxi_drives_name + " SET \"Dropoff_area\" = " + str(zone['gid']) + " WHERE id = " + str(point['id']) + ";"
+                    curs.execute(sql)
+                    print('Done dropoff' + str(point['id']))
+                    #print("UPDATE " + taxi_drives_name + " SET \"Dropoff_area\" = " + str(zone['gid']) + " WHERE id = " + str(point['id']) + ";")
+                    break
 
 if __name__ == "__main__":
 
     conn = get_info.connect_to_db()
 
-    #Add id to our green_taxi table
-    add_id = "ALTER TABLE green_taxi ADD COLUMN id SERIAL PRIMARY KEY"
+
 
     curs = conn.cursor()
+
+    #Add id to our green_taxi table
+    add_id = "ALTER TABLE green_taxi ADD COLUMN id SERIAL PRIMARY KEY"
+    curs.execute(sql)
+    conn.commit()
 
     calculate_zones_for_dropoff(conn, curs)
     calculate_zones_for_pickup(conn, curs)
