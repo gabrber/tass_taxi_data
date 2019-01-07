7b7e5e89dd5bb917254cbf54cc16a81b16c00431
gabrber <gabrielaberecka@gmail.com>
2019-01-07 01:20:54 +0100

    move first_run_scripts from calculate_zone.py

--- processing_scripts/calculate_zone.py
+++ processing_scripts/calculate_zone.py
@@ -106,33 +106,10 @@ def calculate_zones_for_poi(conn,curs):
 
     conn.commit()
 
-def first_run(conn,curs):
-
-    # ONLY FIRST TIME - Add id to our green_taxi table
-    add_id = "ALTER TABLE green_taxi ADD COLUMN id SERIAL PRIMARY KEY"
-    curs.execute(add_id)
-    conn.commit()
-
-    # add_area poi
-    add_area_poi = "ALTER TABLE poi ADD COLUMN poi_area Integer"
-    curs.execute(add_area_poi)
-    conn.commit()
-
-    create_poi_point_sql = "SELECT AddGeometryColumn('poi', 'poi_point', 4326, 'POINT', 2);"
-    populate_poi_point_sql = "UPDATE poi SET poi_point = ST_SETSRID(ST_MakePoint(\"POI_LONGITUDE\", \"POI_LATITUDE\"), 4326);"
-    curs.execute(create_poi_point_sql)
-    curs.execute(populate_poi_point_sql)
-    conn.commit()
-
 if __name__ == "__main__":
 
     conn = get_info.connect_to_db()
-
     curs = conn.cursor()
-
-#TODO - move first_run to separate script
-
-    #first_run(conn,curs)
 
     #calculate_zones_for_dropoff(conn, curs)
     #calculate_zones_for_pickup(conn, curs)
