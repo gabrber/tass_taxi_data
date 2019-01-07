26c2ac062862332f5c7292ea20db52cb6d01d5ca
gabrber <gabrielaberecka@gmail.com>
2019-01-07 16:28:11 +0100

    fix to add new data to existing table

--- processing_scripts/filter_table.py
+++ processing_scripts/filter_table.py
@@ -9,9 +9,14 @@ import shapely.wkt
 def create_new_filtered_table(conn, curs, new_table, days, hours):
 
     taxies = get_info.get_tables_pattern("green", conn)
-    taxi_table =taxies[0]
+    new_table_check = get_info.get_tables_pattern(new_table, conn)
+    taxi_table = taxies[0]
 
-    sql = "SELECT * INTO " + new_table + " FROM " + taxi_table + " WHERE (\"Day_of_week\" BETWEEN " + str(days[0]) + " AND " +  str(days[1]) + ") AND " + " (\"Pickup_hour\" BETWEEN " + str(hours[0]) + " AND " + str(hours[1]) + ");"
+    if not new_table_check:
+        sql = "SELECT * INTO " + new_table + " FROM " + taxi_table + " WHERE (\"Day_of_week\" BETWEEN " + str(days[0]) + " AND " +  str(days[1]) + ") AND " + " (\"Pickup_hour\" BETWEEN " + str(hours[0]) + " AND " + str(hours[1]) + ");"
+    else:
+        sql = "INSERT INTO " + new_table + " SELECT * FROM " + taxi_table + " WHERE (\"Day_of_week\" BETWEEN " + str(days[0]) + " AND " +  str(days[1]) + ") AND " + " (\"Pickup_hour\" BETWEEN " + str(hours[0]) + " AND " + str(hours[1]) + ");"
+
     curs.execute(sql)
     conn.commit()
 
@@ -23,4 +28,4 @@ if __name__ == "__main__":
 
     # Monday is 0, Sunday is 6
 
-    create_new_filtered_table(conn,curs,"filter_weekend_mornings",(5,6),(8,9))
+    create_new_filtered_table(conn,curs,"filter_weekendss_mornings",(5,6),(10,11))
